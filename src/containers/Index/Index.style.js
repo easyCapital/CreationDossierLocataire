d855b7011cad6967057ee61aad65beb7b0749dde
stratsrv66 <stratsrvyt@gmail.com>
2022-02-09 11:10:32 +0100

    09 02 2022 1110 | Responsive pt1

--- src/containers/Index/Index.style.js
+++ src/containers/Index/Index.style.js
@@ -35,4 +35,80 @@ export const IndexWrapper = styled.div`
   position: relative;
   bottom: 45px;
 }
+
+@media screen and (max-width: 1500px)
+{
+
+  .upper{
+    margin-top: 150px;
+    text-align: center;
+    h2{
+      font-size: 60px;
+    }
+    .seconds{
+      position: relative;
+      bottom: 45px;
+      color: #005fc3;
+    }
+    h3{
+      font-size: 20px;
+      margin: 10px 100px;
+      position: relative;
+      bottom: 45px;
+    }
+    .btns{
+      display: flex;
+    }
+  }
+  
+  .btns_2{
+    margin-top: 60px;
+  }
+  .home_btn{
+    margin: 10px 20px;
+    font-size: 18px;
+    padding: 30px 22px;
+    background: #005fc3;
+    position: relative;
+    bottom: 45px;
+  }
+  
+}
+
+@media screen and (max-width: 580px)
+{
+
+  .upper{
+    margin-top: 150px;
+    text-align: center;
+    h2{
+      font-size: 60px;
+    }
+    .seconds{
+      position: relative;
+      bottom: 45px;
+      color: #005fc3;
+    }
+    h3{
+      font-size: 20px;
+      margin: 10px 100px;
+      position: relative;
+      bottom: 45px;
+    }
+    .btns{
+      display: inline-block;
+    }
+  }
+  
+  .btns_2{
+    margin-top: 60px;
+  }
+  .home_btn{
+    margin: 10px 20px;
+    font-size: 18px;
+    padding: 30px 22px;
+    background: #005fc3;
+  }
+  
+}
 `;
