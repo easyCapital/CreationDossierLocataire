51adaece7ebdc7de8c44ee10b01cfe3673cf3ad8
Benoit <bcardinault@easycapital.fr>
2022-07-12 09:53:36 +0200

    sentry added

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-const withAntdLess = require('next-plugin-antd-less');
+const { withSentryConfig } = require("@sentry/nextjs");
+const withAntdLess = require("next-plugin-antd-less");
 
-module.exports = withAntdLess({
-  lessVarsFilePath: './src/styles/variables.less',
+const moduleExports = withAntdLess({
+  lessVarsFilePath: "./src/styles/variables.less",
 
   webpack(config) {
     return config;
   },
 });
+
+module.exports = withSentryConfig(moduleExports, {});
