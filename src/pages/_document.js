b9e84233111960330066039ea8b951e6537e26f5
Benoit <bcardinault@easycapital.fr>
2022-07-13 10:49:17 +0200

    google analytic added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,8 +11,28 @@ class MyDocument extends Document {
           />
           <script
             type="text/javascript"
-            src={"https://maps.googleapis.com/maps/api/js?key=" + process.env.GOOGLE_API_KEY + "&libraries=geometry,places"}
+            src={
+              "https://maps.googleapis.com/maps/api/js?key=" +
+              process.env.GOOGLE_API_KEY +
+              "&libraries=geometry,places"
+            }
             defer
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
